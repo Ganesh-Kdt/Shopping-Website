4de514820e9a269c203be1426c699132c9dc1bda
Ganesh-Kdt <gkudtarkar8@gmail.com>
2021-08-18 01:27:33 +0530

    third change

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
